8845164ca587ace15aeafeaca12a741e4f3dafc8
Nozerone <134429390+Nozerone@users.noreply.github.com>
2023-08-25 22:10:22 -0400

    further developed files

---
+++ tests/Triangle.test.js
@@ -0,0 +1,1 @@
+const triangle = require('../triangle');
---
+++ utils/index.js
@@ -0,0 +1,63 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generate = require('./utils/generate.js');
+
+const questions = [
+
+{
+type: 'Input',
+message: 'enter a set of 3 charachters for your logo',
+name: 'char_text'
+},
+
+{
+    type: 'input',
+    message: 'Please enter a hexadecimal number color for the text?',
+    name: 'char_Color',
+},
+
+{
+    type: 'listraw',
+    message: 'Please select a shape from the list',
+    name: 'shape_logo',
+    choices: [
+        circle, 
+        triangle, 
+        square
+    ],
+},
+
+{
+    type: 'input',
+    mesage: 'Please enter a hexadecimal number for the color of your chosen shape',
+    name: 'shape_Color'
+},
+
+
+]
+
+
+//Function to write file
+
+function writeToFile(fileName, data) {
+    var content = generare(data);
+    fs.writeFile(fileName, content, function(error) {
+        if (error) {
+            return console.log(error);
+        }
+    })
+}
+
+console.log('Gerated logo.sgv');
+
+//Function to initiate app
+
+function init() {
+    inquirer.prompt(questions).then(function(data) {
+        var filename = 'logo.svg';
+        writeToFile(fileName, data);
+    });
+}
+
+
+init();
